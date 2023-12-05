c4beac5daa9ffed6e28cf901f1fc59dd60196501
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2023-12-05 20:49:05 +0200

    starting session added

--- CSCB525_transport_company/src/main/java/org/example/Main.java
+++ CSCB525_transport_company/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.configuration.SessionFactoryUtil;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        SessionFactoryUtil.getSessionFactory().openSession();
     }
 }
