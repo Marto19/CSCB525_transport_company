e2e5f10197abfee0e7c93c2cdc907ba6dc006f17
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2023-12-15 16:24:57 +0200

    created goods and goodsType

---
+++ CSCB525_transport_company/src/main/java/org/example/entity/Goods.java
@@ -0,0 +1,64 @@
+package org.example.entity;
+
+import org.example.enums.GoodsType;
+import org.jetbrains.annotations.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "goods")
+public class Goods {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @NotNull
+    private long id;
+    @Column(name = "weight")
+    @NotNull
+    private double weight;
+    @Enumerated
+    @NotNull
+    private GoodsType goodsType;
+
+    public Goods(@NotNull double weight, @NotNull GoodsType goodsType) {
+        this.weight = weight;
+        this.goodsType = goodsType;
+    }
+
+    public Goods(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public GoodsType getGoodsType() {
+        return goodsType;
+    }
+
+    public void setGoodsType(GoodsType goodsType) {
+        this.goodsType = goodsType;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "id=" + id +
+                ", weight=" + weight +
+                ", goodsType=" + goodsType +
+                '}';
+    }
+
+    //TODO: maybe remove all the setters for the id in all classes
+}
