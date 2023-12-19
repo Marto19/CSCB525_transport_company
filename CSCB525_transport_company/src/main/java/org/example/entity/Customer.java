5f0ea2addc7a88316fe63d6bf2058dcaa6966ce8
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2023-12-19 17:30:42 +0200

    finishing Customer

--- CSCB525_transport_company/src/main/java/org/example/entity/Customer.java
+++ CSCB525_transport_company/src/main/java/org/example/entity/Customer.java
@@ -17,4 +17,52 @@ public class Customer {
     @OneToMany(mappedBy = "customer")
     private List<OrderDetails> orderDetailsList = new ArrayList<>();
 
+    public Customer(String firstName, String lastName, List<OrderDetails> orderDetailsList) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.orderDetailsList = orderDetailsList;
+    }
+    public Customer(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<OrderDetails> getOrderDetailsList() {
+        return orderDetailsList;
+    }
+
+    public void setOrderDetailsList(List<OrderDetails> orderDetailsList) {
+        this.orderDetailsList = orderDetailsList;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", orderDetailsList=" + orderDetailsList +
+                '}';
+    }
 }
