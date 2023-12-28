fd8f9197066d0ed76a0f70628c8a5bbb2aa5d4b2
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2023-12-28 18:31:27 +0200

    adding javax.validation:validation-api:2.0.1.Final

--- CSCB525_transport_company/build.gradle.kts
+++ CSCB525_transport_company/build.gradle.kts
@@ -21,11 +21,13 @@ dependencies {
 
     implementation("org.jetbrains:annotations:24.0.0")
     implementation("org.jetbrains:annotations:24.0.0")
-    implementation ("org.hibernate:hibernate-core:5.6.15.Final")
-    implementation ("mysql:mysql-connector-java:8.0.18")
-    implementation ("org.apache.logging.log4j:log4j-core:2.12.1")
+    implementation("org.hibernate:hibernate-core:5.6.15.Final")
+    implementation("mysql:mysql-connector-java:8.0.18")
+    implementation("org.apache.logging.log4j:log4j-core:2.12.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("javax.validation:validation-api:2.0.1.Final")
+
 }
 
 tasks.test {
--- CSCB525_transport_company/src/main/java/org/example/entity/TransportCompany.java
+++ CSCB525_transport_company/src/main/java/org/example/entity/TransportCompany.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
-import org.hibernate.annotations.Entity;
+import javax.persistence.Entity;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
