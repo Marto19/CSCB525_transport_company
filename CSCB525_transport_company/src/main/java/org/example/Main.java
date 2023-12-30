afe3d20e461ffdb1ff1bc34f6c688bee4eabf1a0
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2023-12-30 22:00:13 +0200

    Update Main.java

--- CSCB525_transport_company/src/main/java/org/example/Main.java
+++ CSCB525_transport_company/src/main/java/org/example/Main.java
@@ -42,6 +42,4 @@ public class Main {
 
 //TODO: add client table, think about it, connect it with the payments - done
 //TODO: watch out for those DTO's properties. You will have to redo them with the properties you need
-//TODO: change the enums by making them entities and connect them -done for GoodsType, but not connected
-// i think to be able to connect them, first u would have to create a DAO with thich to create a position
-// after that, create position in main and then you would be able to connect them?
+//TODO: change the enums by making them entities and connect them - done for GoodsType
